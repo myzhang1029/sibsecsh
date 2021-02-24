b64e6f3d8fad2633ac02db5a59a9eaf164b8c16a
Zhang Maiyun <myzhang1029@hotmail.com>
2021-02-24 18:00:24 +0800

    Add license notice and change license to AGPL

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,21 @@
+//
+//  Copyright (C) 2021 Zhang Maiyun <myzhang1029@hotmail.com>
+//
+//  This file is part of sib secure shell.
+//
+//  Sib secure shell is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Sib secure shell is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with sib secure shell.  If not, see <https://www.gnu.org/licenses/>.
+//
 use crate::ip::get_from_ip;
 use exec::Command;
 use serde::Deserialize;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 //! sibsecsh is a two factor authentication application designed to be used
 //! as the login shell. It acts as a wrapper around the actual shell process.
+//
+//  Copyright (C) 2021 Zhang Maiyun <myzhang1029@hotmail.com>
+//
+//  This file is part of sib secure shell.
+//
+//  Sib secure shell is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Sib secure shell is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with sib secure shell.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 extern crate home;
 extern crate ipaddress;
